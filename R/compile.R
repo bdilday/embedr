b972053bdcf00eb5c23158501b357862b6910317
Lance Bachmeier <lanceb@fastmail.com>
2016-10-04 16:29:26 -0500

    Started work on allowing D inline functions like Rcpp.

--- R/compile.R
+++ R/compile.R
@@ -51,3 +51,75 @@ dmd <- function(name, dlibs="", other="", run=TRUE) {
     system(cmd)
   }
 }
+
+compile <- function(code, libname, deps="", other="") {
+	dcode <- paste0('import core.runtime;
+struct DllInfo;
+
+extern(C) {
+	void R_init_lib', libname, '(DllInfo * info) {
+		Runtime.initialize();
+	}
+	
+	void R_unload_lib', libname, '(DllInfo * info) {
+		Runtime.terminate();
+	}
+}
+
+', code, '
+}
+')
+	# Save code to file with temporary name
+	module <- paste0(find.package("embedr")[1], "/embedr/r.d")
+	libr <- system("locate -b '\\libR.so' -l 1", intern=TRUE)
+	
+	# Compilation line when there are no dependencies
+	cmd <- paste0("dmd -c lib", libname, ".d -fPIC ", module, " -L", libr)
+	
+	# Now we need to get information about any dependencies
+	# Add compilation information about any additional packages
+	if (!isTRUE(dlibs == "")) {
+		# The while loop is used to load dependencies of dependencies
+		# Keeps pulling dependency information until there are no new dependencies
+		v <- computeLibs(dlibs)
+		difference <- 1
+		while (difference > 0) {
+			v.start <- v
+			v <- computeLibs(v.start)
+			difference <- length(v) - length(v.start)
+		}
+		allLibs <- v
+		
+		for (dlib in allLibs) {
+			m <- getExportedValue(dlib, "modules")()
+			mdir <- getExportedValue(dlib, "moddir")()
+			mod <- paste0(find.package(dlib)[1], "/", mdir, "/", m, ".d", sep="", collapse=" ")
+			flags <- getExportedValue(dlib, "flags")()
+			fullAddition <- paste0(" ", mod, " ", flags)
+			cmd <- paste0(cmd, fullAddition)
+		}
+	}
+	
+	# Now that we have added dependency information to the compilation line, we can do the first compilation
+	print(cmd)
+	out1 <- system(cmd, intern=TRUE)
+	print(out1)
+	
+	# Create the .so
+	cmd2 <- paste0("dmd -oflib", libname, ".so lib", libname, ".o -shared -defaultlib=libphobos2.so");
+	print(cmd2)
+	out2 <- system(cmd2, intern=TRUE)
+	print(out2)
+	
+	# Load the .so
+	dyn.load(paste0("lib", libname, ".so"))
+}
+
+compileFile <- function(filename, libname, deps="", other="") {
+	compile(paste(readLines(filename), collapse="\n"), libname, deps, other)
+}
+
+reload.embedr <- function() {
+	detach("package:embedr", unload = TRUE)
+  library(embedr)
+}
