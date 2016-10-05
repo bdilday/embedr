ad2cfd7b6394932c3b0d28c6538275b38e925f33
Lance Bachmeier <lanceb@fastmail.com>
2016-10-05 14:18:38 -0500

    I have inline functions working with dmdgretl. Can improve the function to use other libraries later.

--- R/compile.R
+++ R/compile.R
@@ -52,8 +52,9 @@ dmd <- function(name, dlibs="", other="", run=TRUE) {
   }
 }
 
+# For right now, this only accommodates files that make use of Gretl
 compile <- function(code, libname, deps="", other="") {
-	dcode <- paste0('import core.runtime;
+	cat('import core.runtime;
 struct DllInfo;
 
 extern(C) {
@@ -66,50 +67,25 @@ extern(C) {
 	}
 }
 
-', code, '
-}
-')
+', 
+code, 
+'
+', file="__tmp__compile__file.d", sep="")
 	# Save code to file with temporary name
-	module <- paste0(find.package("embedr")[1], "/embedr/r.d")
-	libr <- system("locate -b '\\libR.so' -l 1", intern=TRUE)
+	apiModule <- paste0(find.package("embedr")[1], "/inline/libembedr.d")
+#	libr <- system("locate -b '\\libR.so' -l 1", intern=TRUE)
 	
-	# Compilation line when there are no dependencies
-	cmd <- paste0("dmd -c lib", libname, ".d -fPIC ", module, " -L", libr)
+	# compile fPIC and so
+	cmd.fpic <- paste0("dmd -c __tmp__compile__file.d -fPIC ", apiModule, dmdgretl::fpicIncludes())
+	cmd.so <- paste0("dmd -oflib", libname, ".so __tmp__compile__file.o libembedr.o ", dmdgretl::soFlags(), " -shared -defaultlib=libphobos2.so");
+
+	print(cmd.fpic)
+	out.fpic <- system(cmd.fpic, intern=TRUE)
+	print(out.fpic)
 	
-	# Now we need to get information about any dependencies
-	# Add compilation information about any additional packages
-	if (!isTRUE(dlibs == "")) {
-		# The while loop is used to load dependencies of dependencies
-		# Keeps pulling dependency information until there are no new dependencies
-		v <- computeLibs(dlibs)
-		difference <- 1
-		while (difference > 0) {
-			v.start <- v
-			v <- computeLibs(v.start)
-			difference <- length(v) - length(v.start)
-		}
-		allLibs <- v
-		
-		for (dlib in allLibs) {
-			m <- getExportedValue(dlib, "modules")()
-			mdir <- getExportedValue(dlib, "moddir")()
-			mod <- paste0(find.package(dlib)[1], "/", mdir, "/", m, ".d", sep="", collapse=" ")
-			flags <- getExportedValue(dlib, "flags")()
-			fullAddition <- paste0(" ", mod, " ", flags)
-			cmd <- paste0(cmd, fullAddition)
-		}
-	}
-	
-	# Now that we have added dependency information to the compilation line, we can do the first compilation
-	print(cmd)
-	out1 <- system(cmd, intern=TRUE)
-	print(out1)
-	
-	# Create the .so
-	cmd2 <- paste0("dmd -oflib", libname, ".so lib", libname, ".o -shared -defaultlib=libphobos2.so");
-	print(cmd2)
-	out2 <- system(cmd2, intern=TRUE)
-	print(out2)
+	print(cmd.so)
+	out.so <- system(cmd.so, intern=TRUE)
+	print(out.so)
 	
 	# Load the .so
 	dyn.load(paste0("lib", libname, ".so"))
