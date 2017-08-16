2b7a62f9285b6c487fd01d93b571a85d3ae38d40
Lance Bachmeier <lanceb@fastmail.com>
2017-08-16 11:07:10 -0500

    Added first Windows support

--- R/compile.R
+++ R/compile.R
@@ -1,3 +1,11 @@
+reload.embedr <- function() {
+	detach("package:embedr", unload = TRUE)
+  library(embedr)
+}
+
+# These functions are all for Linux systems
+# DMD works for Linux, as does LDC, but LDC support has not yet been added
+# I think it should be trivial, but I need time to test it
 computeLibs <- function(libs) {
 	dependencies <- NULL
 	for (lib in libs) {
@@ -101,7 +109,84 @@ compileFile <- function(filename, libname, deps="", other="", rebuild=FALSE) {
 	compile(paste(readLines(filename), collapse="\n"), libname, deps, other, rebuild)
 }
 
-reload.embedr <- function() {
-	detach("package:embedr", unload = TRUE)
-  library(embedr)
+# These functions are for Windows
+# Only LDC can be used to produce shared libraries on Windows
+defaultCompile <- function(filename) {
+	loc <- paste0(path.expand("~"), "/embedr")
+	compiler <- paste0('"', loc, '/ldc/ldc2-1.3.0-beta2-win64-msvc/bin/ldmd2.exe"')
+	module <- paste0('"', loc, '/r.d"')
+	lib <- paste0('"', loc, '/R.lib"')
+	cmd <- paste0(compiler, " -shared -m64 ", filename, ".d ", module, " -version=inline ", lib)
+	print(cmd)
+	msg <- system(cmd, intern=TRUE)
+	print(msg)
+	dyn.load(paste0(filename, ".dll"))
 }
+
+customCompile <- function(filename, path) {
+    loc <- checkSetup(TRUE)
+
+    cmd <- paste0('"', path, '\\ldmd2.exe" -shared -m64 ', filename, '.d "', loc, '\\r.d" -version=inline "', loc, '\\R.lib"')
+    print(cmd)
+    msg <- system(cmd, intern=TRUE)
+    print(msg)
+    dyn.load(paste0(filename, ".dll"))
+}
+
+ldc <- function(filename, path="") {
+	if (path == "") {
+		defaultCompile(filename)
+	} else {
+		customCompile(filename, path)
+	}
+}
+
+ldc.install <- function(download=TRUE) {
+	loc <- paste0(path.expand("~"), "/embedr/ldc")
+	if (!dir.exists(loc)) {
+		dir.create(loc)
+	}
+	if (download) {
+		download.file("https://github.com/ldc-developers/ldc/releases/download/v1.3.0-beta2/ldc2-1.3.0-beta2-win64-msvc.zip", 
+			paste0(loc, "/ldc2-1.3.0-beta2-win64-msvc.zip"))
+	}
+	cat("Unzipping ", loc, "/ldc2-1.3.0-beta2-win64-msvc.zip into ", loc, "\n", sep="")
+	unzip(paste0(loc, "/ldc2-1.3.0-beta2-win64-msvc.zip"), exdir=loc)
+}
+
+embedr.configure <- function() {
+	loc <- paste0(path.expand("~"), "/embedr")
+	if (!dir.exists(loc)) {
+		dir.create(loc)
+	}
+	file.copy(paste0(find.package("embedrwin")[1], "/embedrwin/r.d"), loc, overwrite=TRUE)
+	file.copy(paste0(find.package("embedrwin")[1], "/embedrwin/r.lib"), loc, overwrite=TRUE)
+	file.copy(paste0(R.home(), "/bin/x64/R.dll"), loc, overwrite=TRUE)
+}
+
+checkSetup <- function(output=FALSE) {
+    loc <- paste0(path.expand("~"), "/embedr")
+    if (!dir.exists(loc)) {
+        dir.create(loc)
+    }
+
+    origloc <- getwd()
+    setwd(loc)
+    embedrwinloc <- find.package("embedrwin")[1]
+
+    if (!file.exists("R.lib")) {
+        file.copy(paste0(embedrwinloc, "/embedrwin/r.lib"), loc)
+    }
+    if (!file.exists("r.d")) {
+        file.copy(paste0(embedrwinloc, "/embedrwin/r.d"), loc)
+    }
+    if (!file.exists("R.dll")) {
+        file.copy(paste0(R.home(), "/bin/x64/R.dll"), loc)
+    }
+
+    setwd(origloc)
+    if (output) {
+        loc
+    }
+}
+
